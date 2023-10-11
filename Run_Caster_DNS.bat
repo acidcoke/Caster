7172fab3c43e25c756db192ab9385c7cca85109e
acidcoke <oliver@hinzmann.xyz>
2023-10-11 16:49:20 +0200

    remove debug thing

--- Run_Caster_DNS.bat
+++ Run_Caster_DNS.bat
@@ -2,7 +2,6 @@
 echo Running DNS/DPI from Dragonfly CLI with Natlink.
 
 set currentpath=%~dp0
-echo %~dp0
 
 TITLE Caster: Status Window
 python -m dragonfly load --engine natlink _*.py --no-recobs-messages
